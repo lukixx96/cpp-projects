6aecac32fd3bd1ee0e6a9770ce3611ab42453455
lukammendola <lukammendola@gmail.com>
2024-11-28 15:06:52 +0100

    More Flow Control tested (2)
    I added some outputs of previous sections for better readability then I tested break&continue differences and a good usage case of goto

--- _3_Flow_Control/main.cpp
+++ _3_Flow_Control/main.cpp
@@ -37,6 +37,7 @@ private:
 int main() {
 
     // switch case obs
+    std::cout << "Testing switch case with Class with unique implicit conversion to int/enum method" << std::endl;
     Light light(2); 
     switch (light) {  // Conversione implicita da Light a Color
         case Color::Red:
@@ -54,10 +55,40 @@ int main() {
     }
 
     // foreach
+    std::cout << "\nTesting foreach" << std::endl;
     char word[ ] = { 'H', 'e' ,'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '\0' };
     for( char c : word ) std::cout << c; // c <--eqv--> word[i]
     std::cout << std::endl; 
     // for( every element of that type :(in) array of elements of that type  )
+
+    // break and continue
+    std::cout << "\nTesting break and continue:" << std::endl;
+    // expected output is integer number from 10 to 15 and 23
+    int a = 10;
+    do {
+        if( a > 15 && a < 23 ) {
+            a++; // skipping the loop: counter must always be updated 
+            continue;
+        }
+        std::cout << "\tvalue of a: " << a << std::endl;
+        if( a == 23 ) break; // terminate the loop
+        a++;
+    } while( a < 25 );
+
+    // goto
+    std::cout << "\nTesting goto (good usage exemple)" << std::endl;
+    for( int i = 0; i < 10; i++ ){
+        int j = 0;
+        while( i < 8 && j < 10 ){
+            for( int k = 0; k < 20; k+=2 ){
+                std::cout << "(i,j,k) = (" << i << "," << j << "," << k  << ")" << std::endl;
+                if( i == 5 && j == 3 && k == 12) goto STOP;
+            }
+            j++;
+        }
+    }
+    STOP: std::cout << "total break" << std::endl;
+    // a labelled instruction is exectued regardless of being targeted by a goto
 
     return 0;
     
